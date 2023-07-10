10b8a02d18339c73edc7947be60b42de38856422
harshrajeevsingh <harshrajeevsingh@gmail.com>
2023-07-10 15:49:21 +0530

    changes made

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ConnectButton/>
+      <h1>Hii</h1>
     </main>
   )
 }
