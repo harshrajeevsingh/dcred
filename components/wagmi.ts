9059cc8bd1244a14a52c5d45ff5a66af6289ddf7
shikhar360 <sssonishikhar360@gmail.com>
2023-07-10 14:41:46 +0530

    added

---
+++ components/wagmi.ts
@@ -0,0 +1,29 @@
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygon, polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains(
+  [polygonMumbai ],
+  [
+    // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
+    publicProvider(),
+  ]
+);
+
+const walletConnectProjectId = "952483bf7a0f5ace4c40eb53967f1368";
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  projectId: walletConnectProjectId,
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export { wagmiConfig, WagmiConfig, chains, RainbowKitProvider };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import { AppProps } from "next/app";
 
-import type { AppProps } from 'next/app';
-import "./../styles/globals.css";
+import {
+  wagmiConfig,
+  WagmiConfig,
+  RainbowKitProvider,
+  chains,
+} from "../components/wagmi";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
         <Component {...pageProps} />
-     
+        </RainbowKitProvider>
+    </WagmiConfig>
     </>
   );
 }
